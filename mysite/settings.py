120cb498fd6d74c91c21df7893c1303f5ed66e3a
EC2 Default User <ec2-user@ip-172-31-28-49.us-west-2.compute.internal>
2016-03-31 23:04:52 +0000

    working on server

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,4 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = "/home/ec2-user/mysite/polls/static/"
 STATIC_URL = '/static/'
+
